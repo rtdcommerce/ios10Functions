b2f664c6422e0a68a17d08e0eac4718df0e9430c
rtdcommerce <Duncan@Duncans-MacBook-Pro.local>
2016-07-28 15:35:13 -0400

    Functions

---
+++ functions exercise.playground/Contents.swift
@@ -0,0 +1,41 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var num1 = 20
+var num2 = 10
+
+//Adding function passing in parameters (type Double)
+func add(num1: Double, num2: Double) -> Double {
+        return num1 + num2
+}
+
+add(num1: 20, num2: 10)
+
+//subtracting function passing in parameters (type INT)
+func subtract(num1: Int, num2: Int) -> Int {
+    return num1 - num2
+}
+
+subtract(num1: 20, num2: 10)
+
+
+//multiply function passing in parameters of type (Float)
+
+func multiply(num1: Float, num2: Float) -> Float {
+    return num1 * num2
+}
+
+multiply(num1: 20, num2: 10)
+
+
+//Divide function passing in parameters of type (Double)
+
+func divide(num1: Double, num2: Double) -> Double {
+    return num1 / num2
+}
+divide(num1: 20, num2: 10)
+
+
+
+
